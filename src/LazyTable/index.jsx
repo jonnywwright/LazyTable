5209d5be2b900805d7cd60987ed0fb65b4dc703a
zkptcqc <zkptcqc@localhost.com>
2024-03-03 23:03:09 -0500

    Add lazy table folder.

---
+++ src/LazyTable/index.jsx
@@ -0,0 +1,133 @@
+import { useEffect, useState, useRef } from "react";
+import "./index.css";
+
+const incrementSize = 1;
+
+
+/**
+ * **************Load behavior for scrolling*********************
+ * Table should handle fetching. As we scroll into unfilled space
+ * we should fetch to fill up our data reference. While waiting for
+ * data we can block scrolling. Think lazy loading.
+ */
+
+const LazyTable = ({getDataAsync, 
+    fetchSize, 
+    tableHeight, 
+    rowHeight, 
+    totalRecords,
+    mockData}) => {
+  const initialEndBufferSize = (totalRecords * rowHeight) - tableHeight;
+  const recordsPerPage = tableHeight / rowHeight;
+
+  const [data, setData] = useState([]);
+  const [checkedIds, setCheckedIds] = useState([]);
+  const allData = useRef(mockData.map(x => x));
+  const tableContainer = useRef(null);
+  const startBuffer = useRef(null);
+  const endBuffer = useRef(null);
+  const [windowLeft, setWindowLeft] = useState(0);
+
+  // Where the dom manipulation happens.
+  useEffect(() => {
+    const fBufferHeight = windowLeft * rowHeight;
+
+    startBuffer.current.style.height = `${fBufferHeight}px`;
+
+    const eBufferHeight = initialEndBufferSize - fBufferHeight;
+
+    endBuffer.current.style.height = `${eBufferHeight}px`
+  }, [windowLeft])
+
+  // Used for debugging;
+  const onClickLeft = () => {
+    if (windowLeft === 0) {
+      return;
+    }
+
+    const updateWindowLeft = windowLeft - incrementSize;
+
+    setWindowLeft(updateWindowLeft);
+    const right = updateWindowLeft + recordsPerPage;
+    setData(allData.current.slice(updateWindowLeft, right));
+  }
+
+  // Used for debugging;
+  const onClickRight = () => {
+    if (windowLeft + recordsPerPage === totalRecords) {
+      return;
+    }
+
+    const updateWindowLeft = windowLeft + incrementSize;
+    const right = updateWindowLeft + recordsPerPage; 
+
+    setWindowLeft(updateWindowLeft);
+    setData(allData.current.slice(updateWindowLeft, right));
+  } 
+
+  // Load the scroll handler and set the initial data.
+  useEffect(() => {
+    const handler = () => {
+      const scrollTop = tableContainer.current.scrollTop;
+      const left = Math.round(scrollTop/rowHeight);
+      
+      // set the correct data
+      setWindowLeft(left);
+      setData(allData.current.slice(left, left + recordsPerPage));
+    }
+
+    tableContainer.current.style.height = `${tableHeight}px`;
+
+    tableContainer.current.addEventListener("scroll", handler);
+
+    // Set the initial data.
+    setData(allData.current.slice(0, recordsPerPage));
+
+    // Cleanup listeners.
+    return () => {
+      tableContainer.current.removeEventListener("scroll", handler);
+    }
+  }, []);
+  
+  /**
+   * 
+   * Box checking should feel instant. This is a good test to
+   * ensure the rendered row count isn't to high.
+   */
+  const onChecked = (id) => {
+    if (checkedIds.includes(id)) {
+      setCheckedIds(prev => prev.filter(x => x !== id));
+    } else {
+      setCheckedIds(prev => [...prev, id]);
+    }
+  }
+
+  return (
+    <>
+      <button onClick={onClickLeft}>Up</button>
+      <button onClick={onClickRight}>Down</button>
+      <div ref={tableContainer} className='App-Inner'>
+      <div className="animated-gradient" ref={startBuffer}/>
+      <table id="tid">
+        <tbody>
+        {
+          data.map(({a, b, c, d, e}) => 
+            <tr key={a.toString()}>
+              <td><input type="checkbox" checked={checkedIds.includes(a)} onChange={() => onChecked(a)}/></td>
+              <td>{a}</td>
+              <td>{b}</td>
+              <td>{c}</td>
+              <td>{d}</td>
+              <td>{e}</td>
+            </tr>
+          )
+        }
+        </tbody>
+      </table> 
+      <div className="animated-gradient" ref={endBuffer}/>
+      </div>
+    </>
+  );
+}
+
+export default LazyTable;
